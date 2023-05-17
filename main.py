d33c1ef74ff387f15dd60d003afb12c7169206a2
Matt Hutchison <matt.hutchison@gmail.com>
2023-05-16 22:52:00 -0400

    Adds reactive label for displaying text information
    - Refactors some of the main application to make updating the text label
      easier

--- main.py
+++ main.py
@@ -2,30 +2,44 @@
 from leantechniques_demo.request import get_photos
 from textual.app import App, ComposeResult
 from textual.containers import Container, Vertical, VerticalScroll
-from textual.widgets import DataTable, Input, Button
+from textual.reactive import reactive
+from textual.widgets import DataTable, Input, Button, Label
 
-HEADERS = ['ID', 'Album ID', 'Title']
+class ReactiveLabel(Label):
+    '''Label widget with reactive text'''
+    text = reactive('', layout=True)
+
+    def render(self) -> str:
+        return f'{self.text}'
 
 class DisplayApp(App):
     '''Main application class'''
     CSS_PATH = 'layout.css'
+    info = ReactiveLabel('default', id='info')
 
     def compose(self) -> ComposeResult:
         with Container(id='app-grid'):
             with Vertical(id='left-pane'):
-                yield Input(placeholder='Filter by ID', classes='box', id='id')
+                # UI element ids matter on the inputs, they're used as the query parameter name when accessing the API
+                yield Input(placeholder='Filter by Photo ID', classes='box', id='id')
                 yield Input(placeholder='Filter by Album ID', classes='box', id='albumId')
                 yield Input(placeholder='Filter by Title', classes='box', id='title')
                 yield Button('Load Data', id='load_data')
+                yield self.info
+
             with VerticalScroll(id='right-pane'):
                 yield DataTable()
 
     def on_mount(self) -> None:
         table = self.query_one(DataTable)
-        table.add_columns(*HEADERS)
+        table.add_columns('Photo ID', 'Album ID', 'Title')
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         if event.button.id == 'load_data':
+            self.update_table()
+
+    def update_table(self) -> None:
+        try:
             request_filter_inputs = self.query(Input).exclude('#title')
             title_filter = self.query_one('#title').value
             params = {i.id: i.value for i in request_filter_inputs if i.value}
@@ -33,6 +47,10 @@ class DisplayApp(App):
             table = self.query_one(DataTable)
             table.clear()
             table.add_rows(data)
+            self.info.text = f'({len(data)}) rows matching criteria'
+        except Exception as e:
+            self.info.text = f'An error occurred while fetching records.'
+
 
 if __name__ == "__main__":
     app = DisplayApp()
