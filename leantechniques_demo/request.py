df6f10f6934483fbfee9b045dfa3fee25995b3e6
Matt Hutchison <matt.hutchison@gmail.com>
2023-05-16 22:53:29 -0400

    Adds time out for web request since requests is not naturally async. Shouldn't block forever

--- leantechniques_demo/request.py
+++ leantechniques_demo/request.py
@@ -5,6 +5,6 @@ from leantechniques_demo.data import Photo
 SERVICE_URL = 'https://jsonplaceholder.typicode.com/photos'
 
 def get_photos(**params) -> Iterable[Photo]:
-    r = requests.get(SERVICE_URL, params=params)
+    r = requests.get(SERVICE_URL, params=params, timeout=5)
     ret = [Photo(**i) for i in r.json()]
     return ret
